759729d77013468be7101aefceccbc28b965931e
Edward Shin <shinedwardc@gmail.com>
2026-05-07 14:14:19 -0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
+import { Analytics } from "@vercel/analytics/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
